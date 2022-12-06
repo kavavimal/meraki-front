81ca8df45d7ec31be16f4ee91d5c89ad5f887b4a
Vimal <kavavimal@gmail.com>
2022-12-06 12:25:29 +0530

    Changes

---
+++ src/constants/permission.js
@@ -0,0 +1,22 @@
+export const actions = {
+    readAll: 'read_all',
+    readSome: 'read_some',
+    readSelf: 'read_self',
+    read: 'read',
+    create: 'create',
+    update: 'update',
+    delete: 'delete'
+};
+
+export const features = {
+    user: 'User',
+    attendance: 'Attendance',
+    payslip: 'Payslip',
+    document: 'documents',
+    expense: 'Expense',
+    leave: 'Leave',
+    department: 'Department',
+    designation: 'Designation',
+    report: 'Report',
+    setting: 'Setting'
+};
